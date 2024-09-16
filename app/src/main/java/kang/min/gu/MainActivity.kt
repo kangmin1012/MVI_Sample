051e6f1f006a7bbf602f5d24d18f13f9d126df59
kangmin1012 <kangmin1012@naver.com>
2024-09-16 15:16:24 +0900

    UI 작성

--- app/src/main/java/kang/min/gu/MainActivity.kt
+++ app/src/main/java/kang/min/gu/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kang.min.gu.ui.theme.MVI_SampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +33,60 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MVI_SampleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    var textField by remember { mutableStateOf("") }
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Box(
+            modifier = Modifier.fillMaxSize().padding(innerPadding),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                TextField(
+                    value = textField,
+                    onValueChange = { textField = it },
+                    label = { Text("텍스트 입력") }
+                )
+
+                Text("현재 서버에 저장된 값은 \"\" 입니다.")
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Button(
+                        onClick = {}
+                    ) {
+                        Text("저장")
+                    }
+
+                    Button(
+                        onClick = {}
+                    ) {
+                        Text("현재 저장된 값 표시")
+                    }
+                }
+
+
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+@Preview
+fun MainScreenPreview() {
     MVI_SampleTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
